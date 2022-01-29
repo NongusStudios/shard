a33b02982f6c452540611824b1680411a5399805
Nongus Studios <jandrewgadd@gmail.com>
2022-01-29 23:02:29 +1300

    License Added

--- include/shard/gfx/pipeline.hpp
+++ include/shard/gfx/pipeline.hpp
@@ -38,7 +38,8 @@ namespace shard{
         class ShaderModule{
             public:
                 ShaderModule(Device& _device):
-                    device{_device}
+                    device{_device},
+                    _shaderModule{VK_NULL_HANDLE}
                 {}
                 ShaderModule(Device& _device, const char* filePath);
                 ShaderModule(Device& _device, const std::vector<char>& spv);
@@ -153,3 +154,19 @@ namespace shard{
         };
     } // namespace gfx
 } // namespace shard
+
+/**
+    Copyright 2022 Nongus Studios (https://github.com/NongusStudios/shard)
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
